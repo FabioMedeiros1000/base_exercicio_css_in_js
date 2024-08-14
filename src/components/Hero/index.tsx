df78ef8ceb0ee81d71e86ba7e0a34db0b172cce5
FabioMedeiros1000 <fabio.leandro.medeiros@gmail.com>
2024-08-14 16:15:41 -0300

    estilizando com base em styled-components

---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,16 @@
+import { FormHero, FormDiv, TitleHero } from './Hero'
+import { Container } from '../../styles'
+
+const Hero = () => (
+  <FormHero>
+    <FormDiv>
+      <Container>
+        <TitleHero>
+          As melhores vagas para tecnologia, design e artes visuais.
+        </TitleHero>
+      </Container>
+    </FormDiv>
+  </FormHero>
+)
+
+export default Hero
